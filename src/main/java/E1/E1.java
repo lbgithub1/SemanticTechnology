601a4847dfce3850fa6e462e5137833259a45ba3
Louis Boehnlein <lboehnlein01@gmail.com>
2024-05-20 15:38:26 +0200

    E1 Finalisierung

--- src/main/java/E1/E1.java
+++ src/main/java/E1/E1.java
@@ -7,52 +7,54 @@ import org.apache.jena.riot.RDFDataMgr;
 
 public class E1 {
 
-    //Variables are created so taht the code remains dynamic and clear
-    static String mainNamespace = "http://www.uni-trier.de/index.php?id=1890";
-    static String lectureURI = mainNamespace + "/SemanticTechnologies";
-    static String fnBergmann = mainNamespace + "/RalphBergmann";
+    //Variablen werden separat erstellt, damit der Code dynamisch und übersichtlich bleibt
+    static String websiteURI = "http://www.uni-trier.de/index.php?id=1890";
+    static String mainNamespace = "http://www.uni-trier.de/";
+    static String lectureURI = mainNamespace + "SemanticTechnologies";
+    static String fnBergmann = mainNamespace + "RalphBergmann";
     static String emailBergmann = "mailto:bergmann@uni-trier.de";
-    static String fnHoffmann = mainNamespace + "/MaximilianHoffmann";
+    static String fnHoffmann = mainNamespace + "MaximilianHoffmann";
     static String emailHoffmann = "mailto:hoffmann@uni-trier.de";
-    static String creatorURI = mainNamespace + "/creator";
+    static String creatorURI = mainNamespace + "creator";
 
 
     public static void main(String[] args) {
 
-        //Create the model
+        //Modell wird erstellt
         Model model = ModelFactory.createDefaultModel();
 
-        //A (closed) list is created, which represents the three assignments
-        //The list is created at this early stage so that it can be immediately accessed at a later point
+        //Eine (geschlossene) Liste wird erstellt, welche die drei Aufgaben repräsentieren
+        //Die Liste wird bereits hier erstellt, damit sie direkt zu einem späteren Zeitpunkt verfügbar ist
         RDFNode[] assignments = new RDFNode[3];
         assignments[0] = model.createLiteral("1.Assignment");
         assignments[1] = model.createLiteral("2.Assignment");
         assignments[2] = model.createLiteral("3.Assignment");
         RDFList listOfAssignments = model.createList(assignments);
 
-        //Resources are created in chronological order, which are linked directly
-        //Properties are created immediately and not in a separate location for a fast unterstanding of the code
+        //Ressourcen werden in chronologischer Reihenfolge erstellt und direkt miteinander verbunden
+        //Eigenschaften werden erstellt und direkt verwendet, ohne diese zuerst in einer anderen Position zu definieren
         Resource creator = model.createResource(creatorURI)
-                .addLiteral(model.createProperty(mainNamespace + "/fullName"), fnBergmann)
-                .addLiteral(model.createProperty(mainNamespace + "/email"), emailBergmann);
+                .addLiteral(model.createProperty(mainNamespace + "fullName"), fnBergmann)
+                .addLiteral(model.createProperty(mainNamespace + "email"), emailBergmann);
 
 
-        Resource tutor = model.createResource(mainNamespace + "/tutor")
-                .addLiteral(model.createProperty(mainNamespace + "/tutorFullName"), fnHoffmann)
+        Resource tutor = model.createResource(mainNamespace + "tutor")
+                .addLiteral(model.createProperty(mainNamespace + "tutorFullName"), fnHoffmann)
                 .addLiteral(model.createProperty(mainNamespace + "tutorMail"), emailHoffmann);
 
-        Resource websiteUniTrier = model.createResource(mainNamespace)
-                .addProperty(model.createProperty(mainNamespace + "/createdBy"), creator);
+        Resource websiteUniTrier = model.createResource(websiteURI)
+                .addProperty(model.createProperty(mainNamespace + "createdBy"), creator);
 
-        Resource exercise = model.createResource(mainNamespace + "/exercise")
-                .addProperty(model.createProperty(mainNamespace + "/consistsOf"), listOfAssignments)
-                .addProperty(model.createProperty(mainNamespace + "/hasTutor"), tutor);
+        Resource exercise = model.createResource(mainNamespace + "exercise")
+                .addProperty(model.createProperty(mainNamespace + "consistsOf"), listOfAssignments)
+                .addProperty(model.createProperty(mainNamespace + "hasTutor"), tutor);
 
         Resource lecture = model.createResource(lectureURI)
-                .addProperty(model.createProperty(mainNamespace + "/isHeldBy"), creator)
-                .addProperty(model.createProperty(mainNamespace + "/hasExercise"), exercise);
+                .addProperty(model.createProperty(mainNamespace + "isHeldBy"), creator)
+                .addProperty(model.createProperty(mainNamespace + "hasExercise"), exercise);
 
-        //Writing the model to the console
+        //Das Modell wird auf die Konsole geschrieben
+        //Anschließend wird das Modell in den vorgegebenen Validator eingesetzt, um den Graphen zu generieren
         RDFDataMgr.write(System.out, model, Lang.RDFXML);
     }
 }
